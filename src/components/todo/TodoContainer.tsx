9286300d81b0e7ecda1e24d14b0a450ef52ef9aa
rakib-molla <rakibmail1997@gmail.com>
2024-02-11 21:09:25 +0600

    add todo modal 12-4 install shadecn components

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,3 +1,5 @@
+import { Button } from "../ui/button";
+import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./todoCard";
 
 
@@ -5,17 +7,22 @@ import TodoCard from "./todoCard";
 const TodoContainer = () => {
    return (
       <div >
-         <div>
-            <button>Add Todo</button>
-            <button>Filter</button>
+         <div className="flex items-center justify-between p-5">
+            
+            <AddTodoModal/>
+            <Button className="font-semibold">Filter</Button>
          </div>
-         <div className="bg-red-500 w-full  rounded-xl p-5 space-y-3">
+         <div className="bg-primary-gradient w-full  rounded-xl p-1 ">
+
+            <div className="bg-white w-full p-5 rounded-lg space-y-3">
+               <TodoCard/>
+            <TodoCard/>
+            <TodoCard/>
+            </div>
             {/* <div className="bg-white flex items-center justify-center font-bold p-2 rounded-md">
                <p>There is no task available</p>
             </div> */}
-            <TodoCard/>
-            <TodoCard/>
-            <TodoCard/>
+            
          </div>
       </div>
    );
